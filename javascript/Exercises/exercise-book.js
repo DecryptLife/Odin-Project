b73726c19f11ef35b4f1e1b52b5b8fd70f08e201
DecryptLife <benson.thomas2602@gmail.com>
2023-06-13 10:48:33 -0500

    Configuring files

---
+++ javascript/Exercises/exercise-book.js
@@ -0,0 +1,13 @@
+function Books(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function () {
+    console.log(title, " by ", author, ", ", pages, " pages");
+  };
+}
+
+const theHobbit = new Books("The Hobbit", "J.R.R. Tolkien", 295, false);
+theHobbit.info();
--- javascript/Learning/learning.js
+++ javascript/Learning/learning.js
@@ -1,9 +1,14 @@
 function Player(name, marker) {
   this.name = name;
   this.marker = marker;
+
+  this.sayName = function () {
+    console.log(name);
+  };
 }
 
 const player1 = new Player("Lorem", "X");
 const player2 = new Player("Ipsum", "O");
 
 console.log(player1.name, "is the winner");
+player2.sayName();
