3f2ca6e4c224418f25a25d34904eb67317d89589
DecryptLife <benson.thomas2602@gmail.com>
2023-06-13 13:53:44 -0500

    Prototyping

--- javascript/Exercises/exercise-book.js
+++ javascript/Exercises/exercise-book.js
@@ -11,3 +11,15 @@ function Books(title, author, pages, read) {
 
 const theHobbit = new Books("The Hobbit", "J.R.R. Tolkien", 295, false);
 theHobbit.info();
+
+const check = Object.getPrototypeOf(theHobbit) === Books.prototype;
+console.log(check);
+console.log(Object.getPrototypeOf(theHobbit));
+console.log(theHobbit.valueOf());
+// console.log(theHobbit.getPrototypeOf());
+
+let y = new Array();
+
+console.log(Object.getPrototypeOf(y));
+
+console.log(y instanceof Array);
