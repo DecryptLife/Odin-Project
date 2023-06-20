8d09b63eddb25c449b873823c564880b43a21551
DecryptLife <benson.thomas2602@gmail.com>
2023-06-20 15:08:12 -0500

    Frontend Design

---
+++ Library/library.js
@@ -0,0 +1,22 @@
+let myLibrary = [];
+
+function Book() {
+  // the constructor
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function openModal() {
+  document.getElementById("addBook").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("addBook").style.display = "none";
+}
+
+if (myLibrary.length == 0) {
+  console.log(empty_div);
+  console.log("Library empty");
+}
